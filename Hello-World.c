e96e9495001bda4d391af76b52a18a921f4f536d
_unknownx69 <141651533+unknownxx69@users.noreply.github.com>
2025-11-19 08:58:41 +0530

    Add files via upload

---
+++ Hello-World.c
@@ -0,0 +1,21 @@
+// Write a C program to print the message: Hello World!
+
+// Input Format
+
+// No input is required.
+
+// Constraints
+
+// N/A
+
+// Output Format
+
+// Single line printing exactly: Hello World!
+
+// Sample Output 0
+
+#include <stdio.h> 
+int main () {
+    printf("Hello World!");
+    return 0;
+}
