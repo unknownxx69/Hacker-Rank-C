09864d491113b44fb5cf2501b75d1d4a0c430b42
Mr Zinna <141651533+unknownxx69@users.noreply.github.com>
2025-12-09 20:51:26 +0530

    Add files via upload

---
+++ Q24. Leap-year.c
@@ -0,0 +1,56 @@
+// A year is considered a leap year if:
+
+// It is divisible by 4.
+// However, if it is divisible by 100, it must also be divisible by 400 to be considered a leap year.
+// Write a program that checks if a given year is a leap year or not, outputs the result in a specific format, and handles invalid years as well.
+
+// Input Format
+
+// A single integer Y representing the year.
+// Constraints
+
+// −10^5 ≤ Y ≤ 10^5
+
+// Output Format
+
+// Print "The given year [Y] is Invalid year." if the year is invalid.
+// Print "Yes, [Y] is a leap year." if the year is a leap year.
+// Print "No, [Y] is not a leap year." otherwise.
+// Sample Input 0
+
+// 2024
+// Sample Output 0
+
+// Yes, 2024 is a leap year.
+// Sample Input 1
+
+// 2001
+// Sample Output 1
+
+// No, 2001 is not a leap year.
+// Sample Input 2
+
+// -234
+// Sample Output 2
+
+// The given year -234 is Invalid year.
+
+
+#include <stdio.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int year;
+    scanf("%d", &year);
+    if (year<0) {
+        printf("The given year %d is Invalid year.", year);
+    }
+    else if (year%4==0) {
+        printf("Yes, %d is a leap year.", year);
+    }
+    else {
+        printf("No, %d is not a leap year.", year);
+    }
+    return 0;
+}
